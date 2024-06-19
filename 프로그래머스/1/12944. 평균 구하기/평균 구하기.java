69fb79c46f7b41e06c567519a7f80e1fd7369c26
YangGeonu <113342888+YangGeoun@users.noreply.github.com>
2024-06-19 12:59:02 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -2,12 +2,10 @@ class Solution {
     public double solution(int[] arr) {
         double answer = 0;
         int sum = 0;
-        double cnt = 0;
         for (int el : arr) {
             sum += el;
-            cnt += 1;
         }
-        answer = sum / cnt;
+        answer = (double) sum / arr.length;
         return answer;
     }
 }
