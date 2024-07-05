1dab66ff4d3771e4ad52a69bfe195817b01d1606
YangGeonu <113342888+YangGeoun@users.noreply.github.com>
2024-07-05 12:52:30 +0900

    [level 3] Title: 숫자 게임, Time: 56.15 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] A, int[] B) {
         int answer = 0;
+        // 정렬 후에 처음부터(작은것부터) 비교하며 B가 크면 answer++ 하고 다음 인덱스 비교
+        // A가 크면 indexA는 두고 indexB만 ++; (지금 B로는 무엇도 이길 수 없어서)
         Arrays.sort(A);
         Arrays.sort(B);
         int indexA = 0;
