8550200e823f21ae35dbc5763b22100e9466112e
YangGeonu <113342888+YangGeoun@users.noreply.github.com>
2024-07-15 23:07:51 +0900

    [level 3] Title: 단속카메라, Time: 10.40 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,14 +2,13 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[][] routes) {
+		int ans = 1;
         Arrays.sort(routes, (o1, o2)->{
 			if(o1[1] == o2[1]) {
 				return o1[0] - o2[0];
 			}
 			return o1[1] - o2[1];
 		});
-		
-		int ans=1;
 		int end = routes[0][1];
 		for(int i = 1; i < routes.length; i++) {
 			if(routes[i][0]>end) {
